820ba2e9bbb1a59d90dd2f916203bb6d59463654
Azizbek <xusanov0511@gmail.com>
2023-05-08 23:58:18 +0500

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Book book = new Book("Java", "Oracle", 100);
+        Book book1 = new Book("Java", "Oracle", 100);
         System.out.println(book);
     }
 }
